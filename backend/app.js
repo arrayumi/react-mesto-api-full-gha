c81e64cb44daa7ca67d58fda4371591b6f9080ba
Anna <thoria@yandex.ru>
2023-07-31 18:16:51 +0300

    feat: добавить краш-тест

--- backend/app.js
+++ backend/app.js
@@ -30,6 +30,12 @@ app.use(cors);
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signup', validateCreateUser, createUser);
 app.post('/signin', validateLogin, login);
 
